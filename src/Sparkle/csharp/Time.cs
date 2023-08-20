716bcaf63ca75b8720014bc333fc5624c1d5831c
MrScautHD <65916181+MrScautHD@users.noreply.github.com>
2023-08-20 10:54:48 +0200

    Update Time.cs

--- src/Sparkle/csharp/Time.cs
+++ src/Sparkle/csharp/Time.cs
@@ -3,20 +3,13 @@ using Raylib_cs;
 namespace Sparkle.csharp; 
 
 public static class Time {
-
-    /// <summary>
-    /// Gets the time elapsed since the last frame in seconds.
-    /// </summary>
+    
+    /// <inheritdoc cref="Raylib.GetFrameTime"/>
     public static float Delta => Raylib.GetFrameTime();
     
-    /// <summary>
-    /// Gets the total elapsed time since the application started in seconds.
-    /// </summary>
+    /// <inheritdoc cref="Raylib.GetTime"/>
     public static double Total => Raylib.GetTime();
 
-    /// <summary>
-    /// Pauses the execution for the specified amount of time in seconds.
-    /// </summary>
-    /// <param name="seconds">The duration to wait in seconds.</param>
+    /// <inheritdoc cref="Raylib.WaitTime"/>
     public static void Wait(double seconds) => Raylib.WaitTime(seconds);
 }
