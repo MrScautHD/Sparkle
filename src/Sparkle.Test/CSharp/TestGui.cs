3510c3858b9231cabf97e230f574945c30df82db
MrScautHD <65916181+MrScautHD@users.noreply.github.com>
2025-06-10 22:20:48 +0200

    Update TestGui.cs

--- src/Sparkle.Test/CSharp/TestGui.cs
+++ src/Sparkle.Test/CSharp/TestGui.cs
@@ -37,7 +37,7 @@ public class TestGui : Gui {
         LabelData textBoxLabelData = new LabelData(ContentRegistry.Fontoe, "", 18, hoverColor: Color.Green);
         LabelData hintTextBoxLabelData = new LabelData(ContentRegistry.Fontoe, "Write...", 18, color: Color.Gray);
         
-        this.AddElement("Text-Box-Button", new TextureTextBoxElement(textBoxData, textBoxLabelData, hintTextBoxLabelData, Anchor.BottomCenter, Vector2.Zero, 10, rotation: 0, clickFunc: () => {
+        this.AddElement("Text-Box-Button", new TextureTextBoxElement(textBoxData, textBoxLabelData, hintTextBoxLabelData, Anchor.Center, new Vector2(0, -60), 10, rotation: 0, clickFunc: () => {
             Logger.Error("BOX1!");
             return true;
         }));
